6362abcc6940a18b279807c283aa8bb7d1b4e050
Martin Kojtal <c0170@rocketmail.com>
2014-08-27 20:11:09 +0800

    [Uvision4] Options addition - common property, debug, utilities

--- uvision4.py
+++ uvision4.py
@@ -168,7 +168,70 @@ class Uvision4(Exporter):
                 'StartAddress' : 0,
                 'Size' : 0,
             }
-            
+        },
+
+        'CommonProperty' : {
+            'UseCPPCompile' : 0,
+            'RVCTCodeConst' : 0,
+            'RVCTZI' : 0,
+            'RVCTOtherData' : 0,
+            'ModuleSelection' : 0,
+            'IncludeInBuild' : 0,
+            'AlwaysBuild' : 0,
+            'GenerateAssemblyFile' : 0,
+            'AssembleAssemblyFile' : 0,
+            'PublicsOnly' : 0,
+            'StopOnExitCode' : 0,
+            'CustomArgument' : 0,
+            'IncludeLibraryModules' : 0,
+        },
+
+        'DebugOption' : {
+            'Simulator' : {
+                'UseSimulator' : 0,
+                'LoadApplicationAtStartup' : 1,
+                'RunToMain' : 1,
+                'RestoreBreakpoints' : 1,
+                'RestoreWatchpoints' : 1,
+                'RestoreMemoryDisplay' : 1,
+                'RestoreFunctions' : 1,
+                'RestoreToolbox' : 1,
+                'LimitSpeedToRealTime' : 0,
+            },
+            'Target' : {
+                'UseTarget' : 1,
+                'LoadApplicationAtStartup' : 1,
+                'RunToMain' : 1,
+                'RestoreBreakpoints' : 1,
+                'RestoreWatchpoints' : 1,
+                'RestoreMemoryDisplay' : 1,
+                'RestoreFunctions' : 1,
+                'RestoreToolbox' : 1,
+                'RestoreTracepoints' : 1,
+                'RestoreTracepoints' : 1,
+                'RestoreTracepoints' : 1,
+            },
+            'RunDebugAfterBuild' : 0,
+            'TargetSelection' : 0,
+        },
+
+        'Utilities' : {
+            'Flash1' : {
+                'UseTargetDll' : 0,
+                'UseExternalTool' : 0,
+                'RunIndependent' : 0,
+                'UpdateFlashBeforeDebugging' : 0,
+                'Capability' : 0,
+                'DriverSelection' : 0,
+            },
+            'bUseTDR' : 1,
+            'Flash2' : 'BIN\CMSIS_AGDI.dll',
+            'Flash3' : 0,
+            'Flash4' : 0,
+            'pFcarmOut' : 0,
+            'pFcarmGrp' : 0,
+            'pFcArmRoot' : 0,
+            'FcArmLst' : 0,
         }
     }
 
@@ -207,6 +270,10 @@ class Uvision4(Exporter):
                     self.set_target_options(v, data, k)
                 elif k == 'TargetOption':
                     self.set_user_options(v, data, k)
+                elif k == 'DebugOption':
+                    raise RuntimeError("Option not supported yet.")
+                elif k == 'Utilities':
+                    raise RuntimeError("Option not supported yet.")
                 else:
                     self.set_specific_settings(v, data, k)
 
@@ -269,7 +336,6 @@ class Uvision4(Exporter):
             data['TargetOption'].update(mcu_def['TargetOption'])
         except KeyError:
             # does not exist, create it
-            # data['TargetOption'] = self.uvision_settings['TargetOption']
             data['TargetOption'].update(mcu_def['TargetOption'])
 
     def generate(self, data, ide):
