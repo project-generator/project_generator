16f766add8c19f9a61c610fc808c123a4561e7b7
Matthew Else <matthewelse1997@gmail.com>
2015-03-21 09:54:47 +0000

    Add pgen command line name

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
     entry_points={
         'console_scripts': [
             "project_generator=project_generator.main:main",
+            "pgen=project_generator.main:main",
         ]
     },
 
