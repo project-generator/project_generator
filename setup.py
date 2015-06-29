525fcd165609176dfd32562d9a584f54837be38f
0xc0170 <c0170@rocketmail.com>
2015-06-29 19:18:49 +0100

    Setup - v0.6.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [str(requirement.req) for requirement in parse_requirements('requ
 
 setup(
     name='project_generator',
-    version='0.6.0',
+    version='0.6.1',
     description='Project generators for various embedded tools (IDE). IAR, uVision, Makefile and many more in the roadmap!',
     author='Martin Kojtal, Matthew Else',
     author_email='c0170@rocketmail.com, matthewelse1997@gmail.com',
