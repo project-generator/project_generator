35aafe82f73c32e2d3db76d2fb3b988a3c4ade01
0xc0170 <c0170@rocketmail.com>
2015-07-08 17:35:26 +0100

    Test project - basic template for project to be tested

---
+++ tests/test_project.py
@@ -0,0 +1,66 @@
+# Copyright 2015 0xc0170
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import shutil
+
+import yaml
+from unittest import TestCase
+
+from project_generator.project import Project
+from project_generator.workspace import PgenWorkspace
+
+project_1_yaml = {
+    'common': {
+        'sources': ['sources/main.cpp'],
+        'includes': ['includes/header1.h'],
+        'macros': ['MACRO1', 'MACRO2'],
+        'target': ['target1'],
+        'core': ['core1'],
+        'tools_supported': ['iar_arm', 'uvision', 'coide', 'unknown'],
+        'output_type': ['exe'],
+        'debugger': ['debugger_1'],
+    }
+}
+
+projects_yaml = {
+    'projects': {
+        'project_1' : ['test_workspace/project_1.yaml']
+    },
+    'settings' : {
+        'definitions_dir': ['notpg/path/somewhere'],
+        'export_dir': ['not_generated_projects']
+    }
+}
+
+class TestProject(TestCase):
+
+    """test things related to the Project class"""
+
+    def setUp(self):
+        if not os.path.exists('test_workspace'):
+            os.makedirs('test_workspace')
+        # write project file
+        with open(os.path.join(os.getcwd(), 'test_workspace/project_1.yaml'), 'wt') as f:
+            f.write(yaml.dump(project_1_yaml, default_flow_style=False))
+        # write projects file
+        with open(os.path.join(os.getcwd(), 'test_workspace/projects.yaml'), 'wt') as f:
+            f.write(yaml.dump(projects_yaml, default_flow_style=False))
+        self.project = Project('project_1',['test_workspace/project_1.yaml'], PgenWorkspace('test_workspace/projects.yaml'))
+
+    def tearDown(self):
+        # remove created directory
+        shutil.rmtree('test_workspace', ignore_errors=True)
+
+    def test_name(self):
+        assert self.project.name == 'project_1'
