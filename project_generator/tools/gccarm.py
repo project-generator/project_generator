664164404eacefd6272192ff4b6a9c8f9511b287
sarahmarshy <sarah.marsh@arm.com>
2015-08-12 13:54:45 -0500

    Merge overwrote gcc changes, fixed

--- project_generator/tools/gccarm.py
+++ project_generator/tools/gccarm.py
@@ -22,7 +22,7 @@ import logging
 import ntpath
 import subprocess
 
-class MakefileGccArm():
+class MakefileGccArm(Tool,Exporter):
 
     # http://www.gnu.org/software/make/manual/html_node/Running.html
     ERRORLEVEL = {
