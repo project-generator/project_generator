cba416450a0d41b8600e2e789a4f13ac0fe412d5
0xc0170 <c0170@rocketmail.com>
2015-08-31 18:00:53 +0100

    CMakeGccArm name specified, plus fixes for paths in cmake

--- project_generator/tools/cmake.py
+++ project_generator/tools/cmake.py
@@ -17,7 +17,7 @@ import copy
 from .tool import Tool, Exporter
 from .gccarm import MakefileGccArm
 
-class CMake(Tool,Exporter):
+class CMakeGccArm(Tool,Exporter):
 
     generated_project = {
         'path': '',
@@ -39,12 +39,28 @@ class CMake(Tool,Exporter):
     def get_toolchain():
         return 'gcc_arm'
 
+    def fix_paths_unix(self, data):
+        # cmake seems to require unix paths
+        # This might do proper handling in the gcc arm, pass there a param (force normpath to unix)
+        for key in ['includes', 'linker_file', 'source_files_c', 'source_files_cpp', 'source_files_s',
+                    'source_files_obj']:
+            paths = []
+            for value in data[key]:
+                paths.append(value.replace('\\', '/'))
+            data[key] = paths
+
     def export_project(self):
         generated_projects = {}
-        output = copy.deepcopy(self.generated_project)
+        generated_projects = copy.deepcopy(self.generated_project)
 
         data_for_make = self.workspace.copy()
         self.exporter.process_data_for_makefile(data_for_make)
 
-        project_path, output['files']['cproj'] = self.gen_file_jinja(
+        self.fix_paths_unix(data_for_make)
+
+        generated_projects['path'], generated_projects['files']['cmakelist'] = self.gen_file_jinja(
             'cmakelist.tmpl', data_for_make, 'CMakeLists.txt', data_for_make['output_dir']['path'])
+        return generated_projects
+
+    def get_generated_project_files(self):
+        return {'path': self.workspace['path'], 'files': [self.workspace['files']['cmakelist']]}
