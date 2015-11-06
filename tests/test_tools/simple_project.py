fdcd4cce39b654d78c02b2ee9d6e256e9adcce48
Martin Kojtal <c0170@rocketmail.com>
2015-11-06 19:19:34 +0000

    Tets - tools project - add tool specific option to test

--- tests/test_tools/simple_project.py
+++ tests/test_tools/simple_project.py
@@ -4,6 +4,44 @@ project_1_yaml = {
         'includes': ['includes/header1.h'],
         'target': ['mbed-lpc1768'],
         'linker_file': ['linker_script'],
+    },
+    'tool_specific': {
+        'iar': {
+            'macros': ['IAR_TEST_MACRO'],
+            'misc': {
+                'c_flags': ['c_flag_test'],
+                'asm_flags': ['asm_flag_test'],
+                'cxx_flags': ['cxx_flag_test'],
+                'ld_flags': ['ld_flag_test'],
+            }
+        },
+        'uvision': {
+            'macros': ['UVISION_TEST_MACRO'],
+            'misc': {
+                'c_flags': ['c_flag_test'],
+                'asm_flags': ['asm_flag_test'],
+                'cxx_flags': ['cxx_flag_test'],
+                'ld_flags': ['ld_flag_test'],
+            }
+        },
+        'coide': {
+            'macros': ['COIDE_TEST_MACRO'],
+            'misc': {
+                'c_flags': ['c_flag_test'],
+                'asm_flags': ['asm_flag_test'],
+                'cxx_flags': ['cxx_flag_test'],
+                'ld_flags': ['ld_flag_test'],
+            }
+        },
+        'gcc_arm': {
+            'macros': ['GCC_ARM_TEST_MACRO'],
+            'misc': {
+                'c_flags': ['c_flag_test'],
+                'asm_flags': ['asm_flag_test'],
+                'cxx_flags': ['cxx_flag_test'],
+                'ld_flags': ['ld_flag_test'],
+            }
+        }
     }
 }
 
