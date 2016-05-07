4c2a4e9fe239a4bd425487027729bdd0cab80c12
Martin Kojtal <c0170@rocketmail.com>
2016-05-06 19:54:12 -0500

    Setup v0.9.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requirements = [str(requirement.req) for requirement in parse_requirements('requ
 
 setup(
     name='project_generator',
-    version='0.8.16',
+    version='0.9.0',
     description='Project generators for various embedded tools (IDE). IAR, uVision, Makefile and many more in the roadmap!',
     author='Martin Kojtal',
     author_email='c0170@rocketmail.com',
