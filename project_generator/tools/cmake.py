6cf50284b093320ef22d1fa7fb615d288cbd7e15
Jimmy Brisson <theotherjimmy@gmail.com>
2016-06-16 17:18:41 -0500

    Build and test cmake_gcc_arm
    resolves #347

--- project_generator/tools/cmake.py
+++ project_generator/tools/cmake.py
@@ -13,13 +13,25 @@
 # limitations under the License.
 
 import copy
+import logging
+import subprocess
 from os import getcwd
+from os.path import dirname
 
 from .tool import Tool, Exporter
 from .gccarm import MakefileGccArm
 from ..util import SOURCE_KEYS
 
+logger = logging.getLogger('progen.tools.cmake_gcc_arm')
+
 class CMakeGccArm(Tool,Exporter):
+
+    SUCCESSVALUE = 0
+    ERRORLEVEL = {
+        0: 'no errors)',
+        1: 'targets not already up to date',
+        2: 'errors'
+    }
 
     generated_project = {
         'path': '',
@@ -82,3 +94,57 @@ class CMakeGccArm(Tool,Exporter):
 
     def get_generated_project_files(self):
         return {'path': self.workspace['path'], 'files': [self.workspace['files']['cmakelist']]}
+
+    def build_project(self):
+        # cwd: relpath(join(project_path, ("gcc_arm" + project)))
+        # > make all
+        path = dirname(self.workspace['files']['cmakelist'])
+        logging.debug("Building GCC ARM project: %s" % path)
+
+        args = ['cmake', '.']
+        logging.debug(args)
+
+        try:
+            ret_code = None
+            ret_code = subprocess.call(args, cwd=path)
+        except:
+            logging.error("Project: %s build error whilst calling cmake. Is it in your PATH?" % self.workspace['files']['cmakelist'])
+            return -1
+        else:
+            if ret_code != self.SUCCESSVALUE:
+                # Seems like something went wrong.
+                if ret_code < 3:
+                    logging.error("Project: make failed with the status: %s" %
+                                  (self.ERRORLEVEL[ret_code]))
+                else:
+                    logging.error("Project: make failed with unknown error. Returned: %s" %
+                                   (ret_code))
+                return -1
+            else:
+
+                logging.info("CMake succeeded with the status: %s" %
+                             self.ERRORLEVEL[ret_code])
+
+                args = ['make', 'all']
+                logging.debug(args)
+
+                try:
+                    ret_code = None
+                    ret_code = subprocess.call(args, cwd=path)
+                except:
+                    logging.error("Project: build error whilst calling make. Is it in your PATH?")
+                    return -1
+                else:
+                    if ret_code != self.SUCCESSVALUE:
+                        # Seems like something went wrong.
+                        if ret_code < 3:
+                            logging.error("Project: make failed with the status: %s" %
+                                        (self.ERRORLEVEL[ret_code]))
+                        else:
+                            logging.error("Project:make failed with unknown error. Returned: %s" %
+                                        (ret_code))
+                        return -1
+                    else:
+                        logging.info("Build succeeded with the status: %s" %
+                                    self.ERRORLEVEL[ret_code])
+                        return 0
