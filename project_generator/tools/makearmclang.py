7182a14f12a4144fe5ca072ded8ea715cda3d7dc
Chris Reed <flit@me.com>
2020-08-05 17:31:22 -0500

    Added make_armclang tool.

---
+++ project_generator/tools/makearmclang.py
@@ -0,0 +1,42 @@
+# Copyright 2020 Chris Reed
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from copy import deepcopy
+import logging
+
+from .makefile import MakefileTool
+
+logger = logging.getLogger('progen.tools.armclang')
+
+class MakefileArmclang(MakefileTool):
+
+    def __init__(self, workspace, env_settings):
+        MakefileTool.__init__(self, workspace, env_settings, logger)
+
+    @staticmethod
+    def get_toolnames():
+        return ['make_armclang']
+
+    @staticmethod
+    def get_toolchain():
+        return 'armclang'
+
+    def export_project(self):
+        """ Processes misc options specific for AC6, and run generator """
+        generated_projects = deepcopy(self.generated_projects)
+        self.process_data_for_makefile(self.workspace)
+        generated_projects['path'], generated_projects['files']['makefile'] = \
+                self.gen_file_jinja('makefile_armclang.tmpl', self.workspace, 'Makefile',
+                        self.workspace['output_dir']['path'])
+        return generated_projects
--- project_generator/tools_supported.py
+++ project_generator/tools_supported.py
@@ -1,4 +1,5 @@
 # Copyright 2014-2015 0xc0170
+# Copyright (c) 2020 Chris Reed
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,6 +19,7 @@ from .tools.coide import Coide
 from .tools.eclipse import EclipseGnuARM
 from .tools.gccarm import MakefileGccArm
 from .tools.makearmcc import MakefileArmcc
+from .tools.makearmclang import MakefileArmclang
 from .tools.sublimetext import SublimeTextMakeGccARM
 from .tools.gdb import GDB, ARMNoneEABIGDB, JLinkGDB
 from .tools.cmake import CMakeGccArm
@@ -50,7 +52,8 @@ class ToolsSupported:
         'uvision5':             Uvision5,
         'coide':                Coide,
         'make_gcc_arm':         MakefileGccArm,
-        'make_armcc':           MakefileArmcc,
+        'make_armcc':           MakefileArmcc, 
+        'make_armclang':        MakefileArmclang,
         'eclipse_make_gcc_arm': EclipseGnuARM,
         'sublime_make_gcc_arm': SublimeTextMakeGccARM,
         'gdb':                  GDB,
