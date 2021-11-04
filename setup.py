1ad2aade8450303ba5bd572ea6b25d6cae7d68f5
Chris Reed <chris.reed@arm.com>
2021-11-04 10:49:46 -0500

    setup: convert to PEP517; bump pyyaml and Jinja2 versions.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # Copyright 2014-2015 0xc0170
+# Copyright (c) 2021 Chris Reed
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,61 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname), 'r') as f:
-        return f.read()
-
-setup(
-    name='project_generator',
-    description='Project generators for various embedded tools (IDE). IAR, uVision, Makefile and many more in the roadmap!',
-    author='Martin Kojtal',
-    author_email='c0170@rocketmail.com',
-    keywords="c cpp project generator embedded",
-    url="https://github.com/project-generator/project_generator",
-    license="Apache 2.0",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: C",
-        "Programming Language :: C++",
-        "Topic :: Software Development"
-        "Topic :: Software Development :: Embedded Systems",
-    ],
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    use_scm_version={
-        'local_scheme': 'dirty-tag',
-        'write_to': 'project_generator/_version.py'
-    },
-    setup_requires=[
-        'setuptools>=40.0',
-        'setuptools_scm!=1.5.3,!=1.5.4',
-        'setuptools_scm_git_archive',
-        ],
-    install_requires=[
-        'pyyaml>=5.1,<6.0',
-        'Jinja2>2.0<3.0',
-        'xmltodict',
-        'project_generator_definitions>=0.2.2,<0.3.0',
-        ],
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            "project_generator=project_generator.__main__:main",
-            "progen=project_generator.__main__:main",
-        ]
-    },
-    include_package_data=True,
-    zip_safe=True,
-    options={
-        'bdist_wheel': {
-            'universal': True,
-        },
-    },
-)
+setup()
