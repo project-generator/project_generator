project-generator/project_generator

commit 86fcd76cb4fce7fd19881c83f79694bc466ccb7f

List subcommand: default arguments work, added --all.
- The section argument correctly defaults to 'projects'.
- Defaults to projects.yaml file.
- Added --all argument to show all supported possibilities, instead of
  relying on not having a default project file.
- Handle projects with no defined targets.

project_generator/commands/list_projects.py
@@ -24,11 +24,14 @@ help = 'List general progen data as projects, tools or targets'
 
 
 def run(args):
-    if args.file and os.path.exists(args.file):
+    if not args.all:
         generator = Generator(args.file)
         for project in generator.generate():
             if args.section == 'targets':
-                print("%s supports: %s" % (project.name, project.project['target']))
+                try:
+                    print("%s supports: %s" % (project.name, project.project['target']))
+                except KeyError:
+                    print("%s supports: (no targets specified)" % project.name)
             elif args.section == 'projects':
                 print (project.name)
             elif args.section == 'tools':
@@ -36,13 +39,13 @@ def run(args):
                 print("%s supports: %s" % (project.name, ", ".join(tools)))
     else:
         if args.section == 'targets':
-            print("\nProgen supports the following targets:\n")
-            print("\n".join(ProGenTargets().get_targets()))
+            print("Progen supports the following targets:\n")
+            print("\n".join(sorted(ProGenTargets().get_targets())))
         elif args.section == 'tools':
-            print("\nProgen supports the following tools:\n")
-            print("\n".join(ToolsSupported().get_supported()))
+            print("Progen supports the following tools:\n")
+            print("\n".join(sorted(ToolsSupported().get_supported())))
         elif args.section == 'projects':
-            print("\nFile needs to be defined for projects.")
+            print("--all does not apply to projects.")
     return 0
 
 
@@ -51,6 +54,7 @@ def setup(subparser):
                         help='Increase the verbosity of the output (repeat for more verbose output)')
     subparser.add_argument('-q', dest='quietness', action='count', default=0,
                         help='Decrease the verbosity of the output (repeat for more verbose output)')
-    subparser.add_argument("section", choices = ['targets','tools','projects'],
+    subparser.add_argument("section", choices = ['targets','tools','projects'], nargs="?",
                            help="What section you would like listed", default='projects')
-    subparser.add_argument("-f", "--file", help="YAML projects file", type=argparse_filestring_type)
+    subparser.add_argument("-f", "--file", help="YAML projects file", default='projects.yaml', type=argparse_filestring_type)
+    subparser.add_argument("-a", "--all", help="List all available options.", action="store_true")

commit 092a2045dc71724f066ed7827fc05c725a44f417

list: remove projects.yml being default
we support --all for listing everything, projects.yml is not required in that case.
It should be however present if all is not supplied.

86fcd76cb4fce7fd19881c83f79694bc466ccb7f broke the tests. This is fixing it. Do not
check if the file does not exist for all argument. If --all is not defined, we get an
error anyway about projects.yml missing.

project_generator/commands/list_projects.py
@@ -56,5 +56,5 @@ def setup(subparser):
                         help='Decrease the verbosity of the output (repeat for more verbose output)')
     subparser.add_argument("section", choices = ['targets','tools','projects'], nargs="?",
                            help="What section you would like listed", default='projects')
-    subparser.add_argument("-f", "--file", help="YAML projects file", default='projects.yaml', type=argparse_filestring_type)
+    subparser.add_argument("-f", "--file", help="YAML projects file", default='projects.yaml')
     subparser.add_argument("-a", "--all", help="List all available options.", action="store_true")

tests/test_commands/test_list.py
@@ -27,13 +27,6 @@ class TestListCommand(TestCase):
     """test list command"""
 
     def setUp(self):
-        # we produce some files, to check if list projects listing the projects we defined
-        if not os.path.exists('test_workspace'):
-            os.makedirs('test_workspace')
-        # write projects file
-        with open(os.path.join(os.getcwd(), 'test_workspace/projects.yaml'), 'wt') as f:
-            f.write(yaml.dump(projects_yaml, default_flow_style=False))
-
         self.parser = argparse.ArgumentParser()
         subparsers = self.parser.add_subparsers(help='commands')
         self.subparser = subparsers.add_parser('list', help=list_projects.help)
@@ -45,7 +38,7 @@ class TestListCommand(TestCase):
     def test_list_projects(self):
         # For now list projects should not fail, we should add checking stdout if projects match
         list_projects.setup(self.subparser)
-        args = self.parser.parse_args(['list','projects'])
+        args = self.parser.parse_args(['list','projects','--all'])
         result = list_projects.run(args)
 
         assert result == 0
@@ -53,7 +46,7 @@ class TestListCommand(TestCase):
     def test_list_targets(self):
         # For now list projects should not fail, we should add checking stdout if targets match
         list_projects.setup(self.subparser)
-        args = self.parser.parse_args(['list','targets'])
+        args = self.parser.parse_args(['list','targets','--all'])
         result = list_projects.run(args)
 
         assert result == 0
@@ -61,7 +54,7 @@ class TestListCommand(TestCase):
     def test_list_tools(self):
         # For now list projects should not fail, we should add checking stdout if tools match
         list_projects.setup(self.subparser)
-        args = self.parser.parse_args(['list','tools'])
+        args = self.parser.parse_args(['list','tools','--all'])
         result = list_projects.run(args)
 
         assert result == 0